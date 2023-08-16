e416d40e4951b9c32ef08b36d105ca266622b5ac
Fernando Ortiz <t-fernando.ortiz@pantomathglobal.com>
2023-08-16 00:01:57 -0500

    fix  issues date and add car

--- rds/pedido/2023_05_15_asignar_conductor_pedido.sql
+++ rds/pedido/2023_05_15_asignar_conductor_pedido.sql
@@ -12,9 +12,11 @@ DECLARE
     idUser integer;
     idConductor integer;
     newOrden integer;
+    fechaEntregaTimestamp TIMESTAMP; -- Variable para almacenar la fecha como TIMESTAMP
 BEGIN
+    fechaEntregaTimestamp := TO_TIMESTAMP(_fechaEntrega, 'YYYY-MM-DD HH24:MI:SS'); -- Convierte la fecha a TIMESTAMP
     SELECT conductor INTO idConductor FROM carros WHERE _id = _carroId;
-    SELECT orden INTO newOrden FROM pedidos WHERE fechaEntrega = _fechaEntrega AND conductorId = idConductor;
+    SELECT orden INTO newOrden FROM pedidos WHERE fechaEntrega = fechaEntregaTimestamp AND conductorId = idConductor;
     IF newOrden IS NULL THEN newOrden := 0+1; ELSE newOrden := newOrden + 1; END IF;
 
     UPDATE pedidos 
@@ -24,7 +26,7 @@ BEGIN
 
     RETURN new_id::text;
 END;
-$function$
+$function$;
 
 --  SELECT save_pedidos('forma1', 100, 12345,  'frecuencia1',  'lunes', 'martes', '2023-05-15', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ) ;
 
--- rds/pedido/2023_07_10_change_multiple_date_entrega.sql
+++ rds/pedido/2023_07_10_change_multiple_date_entrega.sql
@@ -7,12 +7,15 @@ BEGIN
     FOR i IN 0 .. jsonb_array_length(p_datos) - 1 LOOP
         -- Obtiene el id y valor de cada elemento del array
         DECLARE
-            v_id INT := (p_datos->i->>'_id')::INT;
-            v_fechaentrega VARCHAR := (p_datos->i->>'fechaentrega')::VARCHAR;
+            -- v_id INT := (p_datos->i->>'_id')::INT;
+            -- v_fechaentrega VARCHAR := (p_datos->i->>'fechaentrega')::VARCHAR;
+                v_id INT := (p_datos->i->>'_id')::INT;
+                v_fechaentrega TEXT := p_datos->i->>'fechaEntrega';
+                v_fechaentrega_timestamp TIMESTAMP := TO_TIMESTAMP(v_fechaentrega, 'YYYY-MM-DD HH24:MI:SS');
         BEGIN
             -- Actualiza el usuario con el id correspondiente
             UPDATE pedidos
-            SET fechaEntrega = v_fechaentrega
+            SET fechaEntrega = v_fechaentrega_timestamp
             WHERE _id = v_id;
         END;
     END LOOP;
@@ -20,5 +23,5 @@ END;
 $$;
 
 
--- SELECT change_multiple_date_entrega('[{"_id": 20, "fechaEntrega": "2023-05-17"}, {"_id": 27, "fechaEntrega": "2023-05-17"}]'::jsonb);
+-- SELECT change_multiple_date_entrega('[{"_id": 89949, "fechaEntrega": "2023-05-17"}]'::jsonb);
 
